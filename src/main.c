ad6260a2e8784d67f9a9f6331248dd14b13b1b5d
Nikita Fedorov <fedorov586@gmail.com>
2026-01-06 02:24:31 +0500

    Trim whitespaces

--- src/main.c
+++ src/main.c
@@ -13,18 +13,18 @@ int main()
 
     bool dragWindow = false;
     bool exitWindow = false;
-    
+
     SetConfigFlags(FLAG_WINDOW_UNDECORATED);
     InitWindow(screenWidth, screenHeight, "stegify");
-    
+
     SetWindowPosition(windowPosition.x, windowPosition.y);
-    
+
     SetTargetFPS(60);
 
     while (!exitWindow && !WindowShouldClose())
     {
         mousePosition = GetMousePosition();
-        
+
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !dragWindow)
         {
             if (CheckCollisionPointRec(mousePosition, (Rectangle){ 0, 0, screenWidth, 20 }))
@@ -36,12 +36,12 @@ int main()
         }
 
         if (dragWindow)
-        {            
+        {
             windowPosition.x += (mousePosition.x - panOffset.x);
             windowPosition.y += (mousePosition.y - panOffset.y);
 
             SetWindowPosition((int)windowPosition.x, (int)windowPosition.y);
-            
+
             if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) dragWindow = false;
         }
 
@@ -50,7 +50,7 @@ int main()
             ClearBackground(RAYWHITE);
 
             exitWindow = GuiWindowBox((Rectangle){ 0, 0, screenWidth, screenHeight }, "#198# PORTABLE WINDOW");
-            
+
             DrawText(TextFormat("Mouse Position: [ %.0f, %.0f ]", mousePosition.x, mousePosition.y), 10, 40, 10, DARKGRAY);
             DrawText(TextFormat("Window Position: [ %.0f, %.0f ]", windowPosition.x, windowPosition.y), 10, 60, 10, DARKGRAY);
 
